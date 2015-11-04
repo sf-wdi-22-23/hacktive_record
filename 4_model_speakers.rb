eee0d2a735e0852b185a0b31c5878e6d9416462d
jlopker <jlopker@gmail.com>
2015-11-04 14:58:38 -0800

    commenting

--- 4_model_speakers.rb
+++ 4_model_speakers.rb
@@ -1,13 +1,15 @@
 require 'ffaker'
 require './models/speaker.rb'
 
+# create 5 new speakers
 5.times do
   args = {
             first_name: FFaker::Name.first_name,
             last_name: FFaker::Name.last_name,
             email: FFaker::Internet.free_email
           }
-  speaker = Speaker.create(args)
+  Speaker.create(args)
 end
 
+# show all the speakers
 p Speaker.all
