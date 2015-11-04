c59eff8c1486757c29ce49b071ee50bf0f189298
jlopker <jlopker@gmail.com>
2015-11-04 14:56:51 -0800

    simplifying

--- 4_model_speakers.rb
+++ 4_model_speakers.rb
@@ -5,12 +5,8 @@ require './models/speaker.rb'
   args = {
             first_name: FFaker::Name.first_name,
             last_name: FFaker::Name.last_name,
-            email: FFaker::Internet.free_email,
-            phone: FFaker::PhoneNumber.short_phone_number
+            email: FFaker::Internet.free_email
           }
   speaker = Speaker.create(args)
-end
-
-Speaker.all.each do |speaker|
   p speaker
 end
