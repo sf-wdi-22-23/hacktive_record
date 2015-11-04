5e724ef32f8a70f5abea9b2d6f220ed5ba9b0693
jlopker <jlopker@gmail.com>
2015-11-04 12:39:29 -0800

    typo

--- 4_model_speakers.rb
+++ 4_model_speakers.rb
@@ -2,8 +2,8 @@ require 'ffaker'
 require './models/speaker.rb'
 
 5.times do
-  first = FFaker::Name.last_name
-  last = FFaker::Name.first_name
+  first = FFaker::Name.first_name
+  last = FFaker::Name.last_name
   email = FFaker::Internet.free_email
   # speaker = Speaker.new({})
 end
