ac1b6d325f6ff5315d651f3159b3afdae895665b
jlopker <jlopker@gmail.com>
2015-11-04 20:56:56 -0800

    fixing

--- 5_conference.rb
+++ 5_conference.rb
@@ -1,28 +1,21 @@
-require 'ffaker'
-require './models/speaker.rb'
-require './models/talk.rb'
-
-# SEED DATABASE
+# seed database
 
 5.times do
   Speaker.create({
     first_name: FFaker::Name.first_name,
     last_name: FFaker::Name.last_name,
-    email: FFaker::Internet.free_email,
-    phone: FFaker::PhoneNumber.short_phone_number
+    email: FFaker::Internet.free_email
   })
 end
 
 5.times do
   Talk.create({
     topic: FFaker::Company.catch_phrase,
-    duration: rand(1..6)*15,
-    speaker_id: Speaker.all.sample.id
+    duration: rand(1..6)*15
   })
 end
 
-
-# PRINT TO CONSOLE
+# print to console
 
 puts "Speakers"
 puts "========"
